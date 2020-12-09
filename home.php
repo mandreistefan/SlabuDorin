15dea60fbac6ed954d6efe85304c75a817cdd11f
mandreistefan <m.andrei.stefan@gmail.com>
2020-12-09 23:50:27 +0200

    Initial files

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<head>
+  <title>Numesite</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="home.css">
+</head>
+<html lang="ro">
+<?php include_once("navigation.html")?>
+<body>
+
+</body>
+<div id="big_container" style="background-color:transparent">
+
+</div>
+	<div id="divno2">
+		<h1>Lorem ipsum</h1>
+		<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+	</div>
+<div id="big_container">
+	<div id="elements_container">
+		<div class="element">
+			<img class="overlap_menu_image_series" src="./_img/room_1.png" alt="Produse_img">
+			<span class="element_span"><h3>Lorem ipsum</h3></span>
+			<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+		</div>
+		<div class="element">
+			<img class="overlap_menu_image_series" src="./_img/room_2.png" alt="Produse_img">
+			<span class="element_span"><h3>Lorem ipsum</h3></span>
+			<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+		</div>
+		<div class="element">
+			<img class="overlap_menu_image_series" src="./_img/room_3.png" alt="Produse_img">
+			<span class="element_span"><h3>Lorem ipsum</h3></span>
+			<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+		</div>
+	</div>
+</div>
+
+</html>
+
+<script>
+ipt>
