f97fd8b3210f86ef00064914a08cfa2af4adaff3
mandreistefan <m.andrei.stefan@gmail.com>
2020-12-13 23:00:37 +0200

    Grafic

--- home.php
+++ home.php
@@ -17,38 +17,44 @@
 <div id="big_container" style="background-color:transparent">
 
 </div>
-	<div id="divno2">
-		<h1>Lorem ipsum</h1>
-		<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-	</div>
+
 <div id="big_container">
 	<div id="elements_container">
 		<div class="element">
-			<!--<a class="clickable_element" href="./products.php" >
-				<div class="overlap_element">
-					<img class="overlap_menu_image" src="./_img/room_1.png" alt="Produse_img">
-					<img class="overlay" src="./_img/room_1_2.png" alt="Produse_img">
+			<a class="navigation_element" href="#">
+				<div class="image_container">
+					<img class="overlap_menu_image_series" src="./_img/room_1.png" alt="Produse_img">
 				</div>
-			</a>-->
-			<div class="image_container">
-				<img class="overlap_menu_image_series" src="./_img/room_1.png" alt="Produse_img">
-			</div>
-			<span class="element_span"><h3>Instalatii sanitare</h3></span>
-			<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+				<h1>Instalatii sanitare</h1>
+				<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+			</a>
 		</div>
 		<div class="element">
-			<div class="image_container">
-				<img class="overlap_menu_image_series" src="./_img/room_2.png" alt="Produse_img">
-			</div>
-			<span class="element_span"><h3>Renovari bai.</h3></span>
-			<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+			<a class="navigation_element" href="#">
+				<div class="image_container">
+					<img class="overlap_menu_image_series" src="./_img/room_2.png" alt="Produse_img">
+				</div>
+				<h1 class="element_span">Renovari bai.</h1>
+				<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+			</a>
 		</div>
-		<div>
-			<div class="image_container">
-				<img class="overlap_menu_image_series" src="./_img/room_3.png" alt="Produse_img">
-			</div>	
-			<span class="element_span"><h3>Lorem ipsum</h3></span>
-			<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+		<div class="element">
+			<a class="navigation_element" href="#">
+				<div class="image_container">
+					<img class="overlap_menu_image_series" src="./_img/room_3.png" alt="Produse_img">
+				</div>
+				<h1>Renovari interioare.</h1>
+				<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+			</a>
+		</div>
+		<div class="element">
+			<a class="navigation_element" href="#">
+				<div class="image_container">
+					<img class="overlap_menu_image_series" src="./_img/room_4.png" alt="Produse_img">
+				</div>
+				<span class="element_span"><h1>Renovari interioare.</h1></span>
+				<span class="element_span">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+			</a>
 		</div>
 	</div>
 </div>
