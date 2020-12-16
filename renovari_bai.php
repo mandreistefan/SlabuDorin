d8298121714cb6dfa3c898e393f234803e4f5547
mandreistefan <m.andrei.stefan@gmail.com>
2020-12-16 21:55:47 +0200

    Renovare bai page

---
+++ renovari_bai.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<head>
+  <title>Numesite</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="renovari_interioare.css">
+</head>
+
+<html lang="ro">
+<?php include_once("navigation.html")?>
+<body>
+<div class="jumbotron" style="background-color:#fca311">
+  <div style="display:flex; flex-direction:row; align-items:center">
+	<h1 class="display-4" id="jumbotron_header">Renovari bai</h1>
+  </div>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+</body>
+<div id="big_container">
+	<div style="width:100%">
+		<li class="list-group-item">
+			<ul class="list-group">
+				<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+					<img class="d-flex mr-3 custom_image_main" src="./_img/kitchenfloor_draw.png" alt="Generic placeholder image">
+					<h4>Podele</h1>
+				</li>
+				<li class="list-group-item">Montare gresie.</li>
+			</ul>
+		</li>
+		<li class="list-group-item">
+			<ul class="list-group">
+				<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+					<img class="d-flex mr-3 custom_image_main" src="./_img/kitchentile_draw.png" alt="Generic placeholder image">
+					<h4>Amenajari pereti.</h1>
+				</li>
+				<li class="list-group-item">Montare faianta si altceva.</li>
+				<li class="list-group-item">Inca ceva</li>
+			</ul>
+		</li>
+		<li class="list-group-item">
+			<ul class="list-group">
+				<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+					<img class="d-flex mr-3 custom_image_main" src="./_img/lightbulb_draw.png" alt="Generic placeholder image">
+					<h4>Montarea corpurilor de iluminat.</h1>
+				</li>
+				<li class="list-group-item">Instalatii electrice.</li>
+				<li class="list-group-item">Montare de ceva</li>
+				<li class="list-group-item">Altceva</li>
+			</ul>
+		</li>	
+		<li class="list-group-item">
+			<ul class="list-group">
+				<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+					<img class="d-flex mr-3 custom_image_main" src="./_img/sink_draw.png" alt="Generic placeholder image">
+					<h4>Corpuri sanitare</h4>
+				</li>
+				<li class="list-group-item">Instalatii electrice.</li>
+				<li class="list-group-item">Montare de ceva</li>
+				<li class="list-group-item">Altceva</li>
+			</ul>
+		</li>			
+	</div>
+</div>
+
+</html>
+
+<script>
+ipt>
---
+++ renovari_interioare.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<head>
+  <title>Numesite</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="renovari_interioare.css">
+</head>
+
+<html lang="ro">
+<?php include_once("navigation.html")?>
+<body>
+<div class="jumbotron">
+  <div style="display:flex; flex-direction:row; align-items:center">
+	<img id="jumbotron_logo" src="./_img/logo.png" alt="Produse_img">
+	<h1 class="display-4" id="jumbotron_header">Lorem Ipsum</h1>
+  </div>
+  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  <p class="lead">
+    <a class="btn btn-outline-info btn-lg" href="#" role="button">Learn more</a>
+	<a class="btn btn-outline-info btn-lg" href="#" role="button">Learn more</a>
+	<a class="btn btn-outline-info btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+	<div id="big_container">
+		<div style="width:100%">
+			<li class="list-group-item">
+				<ul class="list-group">
+					<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+						<img class="d-flex mr-3 custom_image_main" src="./_img/floor_draw.png" alt="Generic placeholder image">
+						<h4>Podele</h1>
+					</li>
+					<li class="list-group-item">Montare parchet laminat.</li>
+					<li class="list-group-item">Ce altceva?</li>
+				</ul>
+			</li>
+			<li class="list-group-item">
+				<ul class="list-group">
+					<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+						<img class="d-flex mr-3 custom_image_main" src="./_img/paintbrush_draw.png" alt="Generic placeholder image">
+						<h4>Varuirea si vopsirea peretilor.</h1>
+					</li>
+					<li class="list-group-item">Varuire</li>
+					<li class="list-group-item">Vopsire</li>
+					<li class="list-group-item">Tapetare</li>
+				</ul>
+			</li>
+			<li class="list-group-item">
+				<ul class="list-group">
+					<li class="list-group-item" style="display:flex;flex-direction:row;align-items:center">
+						<img class="d-flex mr-3 custom_image_main" src="./_img/lightbulb_draw.png" alt="Generic placeholder image">
+						<h4>Montarea corpurilor de iluminat.</h1>
+					</li>
+					<li class="list-group-item">Instalatii electrice.</li>
+					<li class="list-group-item">Montare de ceva</li>
+					<li class="list-group-item">Altceva</li>
+				</ul>
+			</li>			
+		</div>
+	</div>
+</body>
+</html>
+
+<script>
+ipt>
